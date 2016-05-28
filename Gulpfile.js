3623fa2ab96f613779cabd67ea6acf80a459dc4f
Axel Hunter <axel@axelhunter.net>
2016-05-28 09:22:03 +0100

    updated images gulp step

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,68 +1,54 @@
 var gulp = require('gulp'),
-	sass = require('gulp-sass'),
-	useref = require('gulp-useref'),
-	uglify = require('gulp-uglify'),
-	gulpIf = require('gulp-if'),
-	minifyCSS = require('gulp-minify-css'),
-	imagemin = require('gulp-imagemin'),
-	cache = require('gulp-cache'),
-	del = require('del'),
-	runSequence = require('run-sequence');
+    sass = require('gulp-sass'),
+    useref = require('gulp-useref'),
+    uglify = require('gulp-uglify'),
+    gulpIf = require('gulp-if'),
+    minifyCSS = require('gulp-minify-css'),
+    imagemin = require('gulp-imagemin'),
+    cache = require('gulp-cache'),
+    del = require('del'),
+    runSequence = require('run-sequence');
 
-gulp.task('default', function (callback) {
-	runSequence(['sass', 'watch'],
-		callback
-	)
+gulp.task('default', function(callback) {
+    runSequence(['sass', 'watch'],
+        callback
+    )
 });
 
-//gulp.task('useref', function(){
-//	return gulp.src('app/*.html')
-//		.pipe(useref())
-//		.pipe(gulpIf('*.css', minifyCSS()))
-//		.pipe(gulpIf('*.js', uglify()))
-//		.pipe(assets.restore())
-//		.pipe(useref())
-//		.pipe(gulp.dest('dist'))
-//});
-
-gulp.task('useref', function () {
-	return gulp.src('app/*.html')
-		.pipe(useref())
-		.pipe(gulpIf('*.css', minifyCSS()))
-		.pipe(gulpIf('*.js', uglify()))
-		.pipe(gulp.dest('dist'));
+gulp.task('useref', function() {
+    return gulp.src('app/*.html')
+        .pipe(useref())
+        .pipe(gulpIf('*.css', minifyCSS()))
+        .pipe(gulpIf('*.js', uglify()))
+        .pipe(gulp.dest('dist'));
 });
 
 gulp.task('sass', function() {
-	return gulp.src('app/scss/**/*.scss') // Gets all files ending with .scss in app/scss and children dirs
-		.pipe(sass())
-		.pipe(gulp.dest('app/css'))
+    return gulp.src('app/scss/**/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('app/css'))
 });
 
-gulp.task('watch', function(){
-	gulp.watch('app/scss/**/*.scss', ['sass']);
+gulp.task('watch', function() {
+    gulp.watch('app/scss/**/*.scss', ['sass']);
 });
 
 gulp.task('clean', function() {
-	del('dist');
+    del('dist');
 });
 
-gulp.task('images', function(){
-	return gulp.src('app/images/**/*.+(png|jpg|jpeg|gif|svg)')
-		.pipe(cache(imagemin({
-			interlaced: true
-		})))
-		.pipe(gulp.dest('dist/images'))
+gulp.task('images', function() {
+    return gulp.src('app/images/**/*.+(png|jpg|jpeg|gif|svg)')
+        .pipe(gulp.dest('dist/images'))
 });
 
 gulp.task('fonts', function() {
-	return gulp.src('app/fonts/**/*')
-		.pipe(gulp.dest('dist/fonts'))
+    return gulp.src('app/fonts/**/*')
+        .pipe(gulp.dest('dist/fonts'))
 });
 
-gulp.task('build', function (callback) {
-	runSequence('clean',
-		['sass', 'useref', 'images', 'fonts'],
-		callback
-	)
+gulp.task('build', function(callback) {
+    runSequence('clean', ['sass', 'useref', 'images', 'fonts'],
+        callback
+    )
 });
